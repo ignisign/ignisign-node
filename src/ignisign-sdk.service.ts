7c0b6e2d907cde647bed5b82e8aadaf36b90b861
TG <thibaud@ignisign.io>
2024-01-31 15:28:37 +0100

    add warning when webhook is missing

--- src/ignisign-sdk.service.ts
+++ src/ignisign-sdk.service.ts
@@ -87,6 +87,24 @@ export class IgnisignSdk extends IgnisignHttpApi {
     super(init);
   }
 
+  public async init(){
+    await super.init();
+    await this.checkWebhookPresence();
+  }
+
+  private async checkWebhookPresence () {
+    const webhooks = await this.getWebhookEndpoints()
+
+    if (!this._init?.disableWebhookWarning && webhooks?.length === 0) {
+      // yellow
+      const color = '\x1b[33m%s\x1b[0m';
+      console.info(color, "[IGNISIGN SDK] You are receiving this warning as you have not set any webhook endpoint.");
+      console.info(color, "[IGNISIGN SDK] You will not be able to receive any webhook from Ignisign.");
+      console.info(color, "[IGNISIGN SDK] Please set at least one webhook endpoint in your Ignisign application settings.");
+      console.info(color, "[IGNISIGN SDK] If you want to disable this warning, please set the disableWebhookWarning property to true in the IgnisignSdkInitializer object.");
+    }
+  } 
+
    /************** APPLICATION *************/
 
    public async getApplicationContext(): Promise<IgnisignApplication_Context> {
